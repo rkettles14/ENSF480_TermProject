0d637a7790a988948fba090ac36bf0aca681aaf9
zacherysims <zach.md.sims97@gmail.com>
2018-11-24 15:30:32 -0700

    Fleshed out Document class
    Now it has a constructor, getters, and setters

--- Document.java
+++ Document.java
@@ -6,8 +6,50 @@
 
 /**
  *
- * @author CHUCK
+ * @author Zach Sims, Rylan Kettles, Brandon Khoo
  */
 public class Document {
-    
+       private String authorName;
+       private String path;
+       private int isbn;
+       private int stockCount;
+
+    public Document(String authorName, String path, int isbn, int stockCount) {
+        this.authorName = authorName;
+        this.path = path;
+        this.isbn = isbn;
+        this.stockCount = stockCount;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(int stockCount) {
+        this.stockCount = stockCount;
+    }
 }
