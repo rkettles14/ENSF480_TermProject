d201b48343e13c616d0679a90cca6a4be57e1d99
Rylan Kettles <rkettles14@gmail.com>
2018-11-24 16:28:41 -0700

    added operator

---
+++ Operator.java
@@ -0,0 +1,47 @@
+
+/**
+ *
+ * @author Rylan, Zach, Brandon
+ */
+public class Operator extends User{
+
+    public Operator(){
+        super();
+        
+    }
+    
+    @Override
+    public void showOptions() {
+        System.out.println("Press 1 to add document.");
+        System.out.println("Press 2 to remove document.");
+        System.out.println("Press 3 to update document.");
+        
+        
+        while(true){
+            String input = stdin.nextLine();
+        
+            if(input.matches("1")){
+                System.out.println("implement later");
+                break;
+                //call add
+            }
+            else if(input.matches("2")){
+                System.out.println("implement later");
+                break;
+                //call remove
+
+            }
+            else if(input.matches("3")){
+                System.out.println("implement later");
+                break;
+                //call update
+            }
+            else{
+                System.out.println("invalid input.");
+
+            }
+        }
+        
+    }
+    
+}
--- User.java
+++ User.java
@@ -1,5 +1,14 @@
+
+import java.util.Scanner;
+
 public  abstract class User {
+    protected Scanner stdin;
     protected Server instance;
+    
+   public User(){
+       stdin = new Scanner(System.in);
+       instance.getInstance();
+   }
     
     /**
      * shows what the user can do to std out
