a8810c90f46a4c054cafd231c9513392e4b865b9
Rylan Kettles <rkettles14@gmail.com>
2018-11-24 14:58:47 -0700

    created user class

---
+++ User.java
@@ -0,0 +1,13 @@
+package ENSF480_TermProject;
+
+
+public  abstract class User {
+    protected Server instance;
+    
+    /**
+     * shows what the user can do to std out
+     * ie "press 1 to add publication"
+     */
+    public abstract void showOptions();
+    
+}
