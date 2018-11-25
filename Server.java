0acb65ee97f9d49c7601f4642f25cd1e9b1d383a
BrandonKhoo <brandonkhoo.bk@gmail.com>
2018-11-24 17:03:00 -0700

    brandons commit

--- Server.java
+++ Server.java
@@ -1,10 +1,19 @@
+import java.util.Vector;
 public class Server {
+	
     private static Server instance;
+    public 
     
-    private Server(){
-        
+    private Server()
+    {
+    	
     }
     
+    private Server(Server src)
+    {
+    	this.users = src.users;
+    	
+    }
     
     public Server getInstance(){
         if(instance != null){
